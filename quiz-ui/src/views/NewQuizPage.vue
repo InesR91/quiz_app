a1b3e66ae5658dce7844ea19675e80449b716191
jimmph <145988248+jimmph@users.noreply.github.com>
2025-09-30 17:24:55 +0200

    debut quiz page

---
+++ quiz-ui/src/views/NewQuizPage.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+// import quizApiService from "@/services/QuizApiService";
+
+onMounted(async () => {
+  console.log('New Quiz page mounted');
+});
+data() {
+  return {
+    username: ''
+  }
+}
+function launchNewQuiz() {
+    console.log('Launch new quiz with', this.username);
+
+}
+</script>
+
+<template>
+  <h1>New quiz page</h1>
+
+  <p>Saisissez votre nom :</p>
+  <input type="text" v-model="username" />
+  <p>{{ Username }}</p>
+  <button @click="launchNewQuiz">Go</button>
+</template>
