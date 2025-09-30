4890687e17eac2a06bfb0b69ebb3486137ba3151
jimmph <145988248+jimmph@users.noreply.github.com>
2025-09-30 10:28:18 +0200

    Login

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from jwt_utils import build_token, decode_token, JwtError
 
 app = Flask(__name__)
 CORS(app)
+
+PASSWORD = "test"
+
 
 @app.route('/')
 def hello_world():
 	x = 'tout le monde'
 	return f"Hello, {x}"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json(force =True)
+	password = payload.get("password", None)
+	if password != PASSWORD:
+		return {"message": "Wrong Password "}, 401
+
+	token = build_token()
+	return {"token": token}, 200 
+	
 
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
